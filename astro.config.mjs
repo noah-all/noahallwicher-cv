39b6c21d67ac5a57a5ecbf7fce148dd1a9666544
Noah Allwicher <nallwicher@yahoo.de>
2025-12-05 11:18:25 +0100

    Fix Astro config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,6 @@
-
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://noah-all.github.io',
   base: '/noahallwicher-cv/',
 });
-
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  // Your main GitHub Pages domain
-  site: 'https://noah-all.github.io',
-  // The repository name, with slashes
-  base: '/noahallwicher-cv/',
-});
-
