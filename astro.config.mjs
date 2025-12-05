7407c5f190f4f0883a8385f0610f95d661440c9a
Noah Allwicher <nallwicher@yahoo.de>
2025-12-05 11:00:13 +0100

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,11 @@
+
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://noah-all.github.io',
+  base: '/noahallwicher-cv/',
+});
+
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
